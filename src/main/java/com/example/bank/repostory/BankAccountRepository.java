d9c784243a96316e0105d2aaac581bf78347b647
Jacen <jacen19931@gmail.com>
2018-09-22 19:02:38 +0300

    дублировало записи

--- src/main/java/com/example/bank/repostory/BankAccountRepository.java
+++ src/main/java/com/example/bank/repostory/BankAccountRepository.java
@@ -2,14 +2,13 @@ package com.example.bank.repostory;
 
 import com.example.bank.domain.AccountId;
 import com.example.bank.entity.BankAccount;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, AccountId> {
 
-    @Override
-    @EntityGraph("abc")
+  //  @Override
+  //  @EntityGraph("abc")
     List<BankAccount> findAll();
 }
